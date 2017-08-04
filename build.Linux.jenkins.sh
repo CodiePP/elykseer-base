60cc2bc142b5339e232bb4147975dfd97aa50855
Alexander V. Diemand <codieplusplus@apax.net>
2017-08-04 15:57:08 +0200

    jenkins job

--- build.Linux.jenkins.sh
+++ build.Linux.jenkins.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
 
 PLATFRM="Any CPU"
 
@@ -8,8 +10,8 @@ nuget restore -PackagesDirectory packages UT\packages.config
 sn -k eLyKseeR.snk
 sn -k UT/ut.snk
 
-xbuild /t:clean /p:Configuration="Debug" /p:Platform=${PLATFRM} eLyKseeR-base.Linux.sln
-xbuild /p:Configuration="Debug" /p:Platform=${PLATFRM} eLyKseeR-base.Linux.sln
+xbuild /t:clean /p:Configuration="Debug" /p:Platform="${PLATFRM}" eLyKseeR-base.Linux.sln
+xbuild /p:Configuration="Debug" /p:Platform="${PLATFRM}" eLyKseeR-base.Linux.sln
 
 packages/NUnit.Runners.lite.2.6.4.20150512/nunit-console.exe -labels UT/bin/Debug/UT.exe
 
