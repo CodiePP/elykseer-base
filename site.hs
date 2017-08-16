e0f51bc4e8cccf120d34a9a33402ec91aa71c671
Alexander V. Diemand <codieplusplus@apax.net>
2017-08-16 15:17:52 +0200

    new version 1.2.0.2 TR1 released

--- site.hs
+++ site.hs
@@ -11,6 +11,10 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
+    match "static/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
     match "html/*" $ do
         route   idRoute
         compile copyFileCompiler
